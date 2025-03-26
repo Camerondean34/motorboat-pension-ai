c031a2f0ced4fe445dffadf37753e45b5ac950bf
Cameron <91594861+Camerondean34@users.noreply.github.com>
2025-03-26 17:28:22 +0000

    Implemented Player movement for boat scene

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,8 +1,31 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var IsIdle = false
+
+func _ready() -> void:
+	$IdleTimer.timeout.connect(_on_idle)
 
 func _physics_process(_delta):
 	var input_direction = Input.get_axis("Left", "Right")
 	velocity.x = input_direction * speed
+	
+	# Set animation
+	if velocity.x != 0:
+		IsIdle = false
+		if !$IdleTimer.is_stopped():
+			$IdleTimer.stop()
+		$AnimatedSprite2D.flip_h = velocity.x > 0
+		$AnimatedSprite2D.animation = "Walk"
+	elif !IsIdle:
+		if $IdleTimer.is_stopped():
+			$IdleTimer.start()
+		$AnimatedSprite2D.animation = "FaceLeft"
+		
+	
 	move_and_slide()
+	
+func _on_idle():
+	IsIdle = true
+	$AnimatedSprite2D.animation = "Idle"
+	$IdleTimer.stop()
