4005b62d30bc4762f45f583a3b8e9ecf9435a854
eeoooue <80429519+eeoooue@users.noreply.github.com>
2025-03-30 14:22:27 +0100

    island scene now features a random pensioner + background combination (1 of 16)

--- Scripts/IslandScene.gd
+++ Scripts/IslandScene.gd
@@ -1,8 +1,35 @@
 extends Node2D
 var PlayerByPensioner = false
 
+@export var pensioner_node: Sprite2D
+@export var pensioner_img_01: Texture2D
+@export var pensioner_img_02: Texture2D
+@export var pensioner_img_03: Texture2D
+@export var pensioner_img_04: Texture2D
+
+@export var background_node: Sprite2D
+@export var island_bg_01: Texture2D
+@export var island_bg_02: Texture2D
+@export var island_bg_03: Texture2D
+@export var island_bg_04: Texture2D
+
+func get_random_pensioner_image() -> Texture2D:
+	var options = [pensioner_img_01, pensioner_img_02, pensioner_img_03, pensioner_img_04]
+	return options[randi() % options.size()]
+	
+	
+func get_random_island_background() -> Texture2D:
+	var backgrounds = [island_bg_01, island_bg_02, island_bg_03, island_bg_04]
+	return backgrounds[randi() % backgrounds.size()]
+
 func _ready() -> void:
 	SceneManager.set_up.emit(self)
+	var backgroundTexture = get_random_island_background();
+	background_node.texture = backgroundTexture;
+	
+	var pensionerImg = get_random_pensioner_image();
+	pensioner_node.texture = pensionerImg;
+	
 
 func _on_prompt_area_body_entered(body: Node2D) -> void:
 	if body is Player:
@@ -17,6 +44,8 @@ func _on_prompt_area_body_exited(body: Node2D) -> void:
 func _input(event: InputEvent) -> void:
 	if PlayerByPensioner and event.is_action_pressed("Interact"):
 		capture_pensioner();
+		
+
 		
 func update_ui() -> void:
 	
